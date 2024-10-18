eefc8fdae60b8ce852fdda7b9f3720c221636f87
axodevjs <axxxegao@gmail.com>
2024-10-18 15:55:07 +0500

    feat: add open sans font

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [fontsLoaded] = useFonts({
+    // Шрифты Nunito
     "Nunito-BlackItalic": require("./assets/fonts/Nunito/Nunito-BlackItalic.ttf"),
     "Nunito-Italic": require("./assets/fonts/Nunito/Nunito-Italic.ttf"),
     "Nunito-Black": require("./assets/fonts/Nunito/Nunito-Black.ttf"),
@@ -26,6 +27,19 @@ export default function App() {
     "Nunito-SemiBoldItalic": require("./assets/fonts/Nunito/Nunito-SemiBoldItalic.ttf"),
     "Nunito-ExtraLight": require("./assets/fonts/Nunito/Nunito-ExtraLight.ttf"),
     "Nunito-SemiBold": require("./assets/fonts/Nunito/Nunito-SemiBold.ttf"),
+    // Шрифты OpenSans
+    "OpenSans-BoldItalic": require("./assets/fonts/OpenSans/OpenSans-BoldItalic.ttf"),
+    "OpenSans-Bold": require("./assets/fonts/OpenSans/OpenSans-Bold.ttf"),
+    "OpenSans-ExtraBoldItalic": require("./assets/fonts/OpenSans/OpenSans-ExtraBoldItalic.ttf"),
+    "OpenSans-ExtraBold": require("./assets/fonts/OpenSans/OpenSans-ExtraBold.ttf"),
+    "OpenSans-Italic": require("./assets/fonts/OpenSans/OpenSans-Italic.ttf"),
+    "OpenSans-LightItalic": require("./assets/fonts/OpenSans/OpenSans-LightItalic.ttf"),
+    "OpenSans-Light": require("./assets/fonts/OpenSans/OpenSans-Light.ttf"),
+    "OpenSans-MediumItalic": require("./assets/fonts/OpenSans/OpenSans-MediumItalic.ttf"),
+    "OpenSans-Medium": require("./assets/fonts/OpenSans/OpenSans-Medium.ttf"),
+    "OpenSans-Regular": require("./assets/fonts/OpenSans/OpenSans-Regular.ttf"),
+    "OpenSans-SemiBoldItalic": require("./assets/fonts/OpenSans/OpenSans-SemiBoldItalic.ttf"),
+    "OpenSans-SemiBold": require("./assets/fonts/OpenSans/OpenSans-SemiBold.ttf"),
   });
 
   useEffect(() => {
--- src/shared/ui/Text/Text.tsx
+++ src/shared/ui/Text/Text.tsx
@@ -1,59 +1,84 @@
 import React from "react";
 import { Text as RNText, StyleSheet, TextProps } from "react-native";
 
-// Определяем типы для fontWeight и italic
-type FontWeight =
-  | "100"
-  | "200"
-  | "300"
-  | "400"
-  | "500"
-  | "600"
-  | "700"
-  | "800"
-  | "900";
-
-// Объект для сопоставления fontWeight и italic с шрифтами Nunito
-const fontWeightMap: Record<string, string> = {
-  "100": "Nunito-ExtraLight",
-  "200": "Nunito-ExtraLight",
-  "300": "Nunito-Light",
-  "400": "Nunito-Regular",
-  "500": "Nunito-Medium",
-  "600": "Nunito-SemiBold",
-  "700": "Nunito-Bold",
-  "800": "Nunito-ExtraBold",
-  "900": "Nunito-Black",
-  "100italic": "Nunito-ExtraLightItalic",
-  "200italic": "Nunito-ExtraLightItalic",
-  "300italic": "Nunito-LightItalic",
-  "400italic": "Nunito-Italic",
-  "500italic": "Nunito-MediumItalic",
-  "600italic": "Nunito-SemiBoldItalic",
-  "700italic": "Nunito-BoldItalic",
-  "800italic": "Nunito-ExtraBoldItalic",
-  "900italic": "Nunito-BlackItalic",
+// Объект для сопоставления стилей шрифтов OpenSans и Nunito
+const fontMap: Record<string, Record<string, string>> = {
+  Nunito: {
+    "100": "Nunito-ExtraLight",
+    "200": "Nunito-ExtraLight",
+    "300": "Nunito-Light",
+    "400": "Nunito-Regular",
+    "500": "Nunito-Medium",
+    "600": "Nunito-SemiBold",
+    "700": "Nunito-Bold",
+    "800": "Nunito-ExtraBold",
+    "900": "Nunito-Black",
+    "100italic": "Nunito-ExtraLightItalic",
+    "200italic": "Nunito-ExtraLightItalic",
+    "300italic": "Nunito-LightItalic",
+    "400italic": "Nunito-Italic",
+    "500italic": "Nunito-MediumItalic",
+    "600italic": "Nunito-SemiBoldItalic",
+    "700italic": "Nunito-BoldItalic",
+    "800italic": "Nunito-ExtraBoldItalic",
+    "900italic": "Nunito-BlackItalic",
+  },
+  OpenSans: {
+    "100": "OpenSans-Light",
+    "200": "OpenSans-Light",
+    "300": "OpenSans-Light",
+    "400": "OpenSans-Regular",
+    "500": "OpenSans-Medium",
+    "600": "OpenSans-SemiBold",
+    "700": "OpenSans-Bold",
+    "800": "OpenSans-ExtraBold",
+    "900": "OpenSans-ExtraBold",
+    "100italic": "OpenSans-LightItalic",
+    "200italic": "OpenSans-LightItalic",
+    "300italic": "OpenSans-LightItalic",
+    "400italic": "OpenSans-Italic",
+    "500italic": "OpenSans-MediumItalic",
+    "600italic": "OpenSans-SemiBoldItalic",
+    "700italic": "OpenSans-BoldItalic",
+    "800italic": "OpenSans-ExtraBoldItalic",
+    "900italic": "OpenSans-ExtraBoldItalic",
+  },
 };
 
 // Кастомный компонент Text
 interface CustomTextProps extends TextProps {
-  weight?: FontWeight;
+  weight?:
+    | "100"
+    | "200"
+    | "300"
+    | "400"
+    | "500"
+    | "600"
+    | "700"
+    | "800"
+    | "900";
   italic?: boolean;
+  family?: "OpenSans" | "Nunito";
 }
 
 const Text: React.FC<CustomTextProps> = ({
   weight = "400",
   italic = false,
+  family = "Nunito",
   style,
   children,
   ...props
 }) => {
-  // Определение правильного шрифта на основе fontWeight и italic
-  const fontKey = italic ? `${weight}italic` : weight;
-  const fontFamily = fontWeightMap[fontKey] || "Nunito-Regular";
+  // Определение правильного шрифта на основе weight, italic и family
+  const fontKey = `${weight}${italic ? "italic" : ""}`;
+  const selectedFontFamily =
+    fontMap[family]?.[fontKey] || fontMap["Nunito"]["400"];
 
   return (
-    <RNText style={[styles.text, style, { fontFamily }]} {...props}>
+    <RNText
+      style={[styles.text, style, { fontFamily: selectedFontFamily }]}
+      {...props}
+    >
       {children}
     </RNText>
   );
