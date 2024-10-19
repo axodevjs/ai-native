0a59430ffdbb2c13531ebdf84554a39e58a5e8a9
senseiGai <swerss564@gmail.com>
2024-10-19 12:03:02 +0500

    feat: analysis mode

--- src/app/screens/ar-screen.tsx
+++ src/app/screens/ar-screen.tsx
@@ -1,30 +1,17 @@
 // ArScreen.jsx
 import React, { useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { Layout } from "../../widgets/ui/layout";
 import AnalysisCard from "../entities/AnalysisCard/analysis-card";
+import { SafeAreaView } from "react-native-safe-area-context";
 // If you're using TypeScript, adjust the import paths accordingly
 
 export const ArScreen = () => {
-  const [activeTab, setActiveTab] = useState("AR");
+  const [activeTab, setActiveTab] = useState("Analysis");
 
   return (
     <Layout>
-      <View className="flex-row border-b border-gray-300 mt-12">
-        <TouchableOpacity
-          className={`flex-1 items-center py-4 ${
-            activeTab === "AR" ? "border-b-2 border-main" : ""
-          }`}
-          onPress={() => setActiveTab("AR")}
-        >
-          <Text
-            className={`text-lg ${
-              activeTab === "AR" ? "text-main font-bold" : "text-gray-500"
-            }`}
-          >
-            AR Mode
-          </Text>
-        </TouchableOpacity>
+      <SafeAreaView className="flex-row border-b border-gray-300">
         <TouchableOpacity
           className={`flex-1 items-center py-4 ${
             activeTab === "Analysis" ? "border-b-2 border-main" : ""
@@ -39,22 +26,34 @@ export const ArScreen = () => {
             Analysis Mode
           </Text>
         </TouchableOpacity>
-      </View>
-      <View className="flex-1 p-4">
-        {activeTab === "AR" && (
-          <View className="flex-1 justify-center items-center">
-            <Text className="text-dark text-xl">AR Mode Content</Text>
-          </View>
-        )}
+        <TouchableOpacity
+          className={`flex-1 items-center py-4 ${
+            activeTab === "AR" ? "border-b-2 border-main" : ""
+          }`}
+          onPress={() => setActiveTab("AR")}
+        >
+          <Text
+            className={`text-lg ${
+              activeTab === "AR" ? "text-main font-bold" : "text-gray-500"
+            }`}
+          >
+            Results
+          </Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+
+      <ScrollView className="flex-1 p-4">
         {activeTab === "Analysis" && (
           <View className="flex-1 justify-center items-center">
-            <View className="items-center">
-              <Text className="text-4xl">Scan you meal.</Text>
-            </View>
             <AnalysisCard />
           </View>
         )}
-      </View>
+        {activeTab === "AR" && (
+          <View className="flex-1 justify-center items-center">
+            <Text className="text-dark text-xl">Results</Text>
+          </View>
+        )}
+      </ScrollView>
     </Layout>
   );
 };
